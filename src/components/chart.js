d6e60fb1775551594062ab913b793846ac863ff0
Andrew Mager <mager@postmates.com>
2016-02-22 17:42:49 -0800

    Add text temperature

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,14 +1,21 @@
 // This should be a function-based component since we aren't using state here. We just need props.
 
 import React from 'react';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import _ from 'lodash';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
 
+
+function average(data) {
+  return _.round(_.sum(data)/data.length);
+}
 
 export default (props) => {
   return (
     <div>
       <Sparklines height={120} width={180} data={props.data}>
         <SparklinesLine color={props.color} />
+        <SparklinesReferenceLine type="avg" />
+        <div>{average(props.data)} {props.units}</div>
       </Sparklines>
     </div>
   );
